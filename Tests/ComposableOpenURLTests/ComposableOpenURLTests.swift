c5f52c6d7fb1148bf8772ebde372e25c77104b2d
Luke Redpath <lredpath@community.com>
2021-03-18 21:51:32 +0000

    Added tests

--- Tests/ComposableOpenURLTests/ComposableOpenURLTests.swift
+++ Tests/ComposableOpenURLTests/ComposableOpenURLTests.swift
@@ -1,15 +1,66 @@
+#if !os(macOS)
 import XCTest
-@testable import ComposableOpenURL
+import ComposableOpenURL
+import ComposableArchitecture
 
-final class ComposableOpenURLTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ComposableOpenURL().text, "Hello, World!")
+class OpenURLTests: XCTestCase {
+    struct AppState: Equatable {
+        var url: URL?
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    enum AppAction: Equatable {
+        case tappedToOpen
+        case openURL(OpenURLViewAction)
+    }
+
+    let store = TestStore(
+        initialState: AppState(),
+        reducer: Reducer<AppState, AppAction, Void> { state, action, _ in
+            switch action {
+            case .tappedToOpen:
+                state.url = URL(string: "http://example.com")
+                return .none
+            case .openURL:
+                return .none
+            }
+        }.opensURL(
+            state: \.url,
+            action: /AppAction.openURL
+        ),
+        environment: ()
+    )
+
+    func testOpeningSupportedURL() {
+        store.assert(
+            .send(.tappedToOpen) {
+                $0.url = URL(string: "http://example.com")
+            },
+            .send(.openURL(.openedURL(true))) {
+                $0.url = nil
+            }
+        )
+    }
+
+    func testOpeningUnsupportedURL() {
+        store.assert(
+            .send(.tappedToOpen) {
+                $0.url = URL(string: "http://example.com")
+            },
+            .send(.openURL(.urlNotSupported)) {
+                $0.url = nil
+            }
+        )
+    }
+
+    func testOpeningURLFails() {
+        store.assert(
+            .send(.tappedToOpen) {
+                $0.url = URL(string: "http://example.com")
+            },
+            .send(.openURL(.openedURL(true))) {
+                $0.url = nil
+            }
+        )
+    }
 }
+#endif
